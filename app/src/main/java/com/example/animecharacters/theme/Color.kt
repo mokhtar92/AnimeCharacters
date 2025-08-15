737a7d5b22252e7861f8a31b15447b902b58880c
Ahmed Mokhtar <ahmed.mokhtar92@gmail.com>
2025-08-15 06:46:05 +0200

    Update app theme

---
+++ app/src/main/java/com/example/animecharacters/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.animecharacters.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val RickBlue = Color(0xFF00B5CC)
+val MortyYellow = Color(0xFFFFD166)
+val PortalGreen = Color(0xFF43D9AD)
+
+// Backgrounds
+val SpaceBlack = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+val SpaceWhite = Color(0xFFFFFFFF)
+val SurfaceLight = Color(0xFFF5F5F5)
+
+// Text
+val TextWhite = Color(0xFFFFFFFF)
+val TextGray = Color(0xFFE1E1E1)
+val TextDarkGray = Color(0xFF2E2E2E)
+
+// Error
+val InterdimensionalRed = Color(0xFFFF6B6B)
